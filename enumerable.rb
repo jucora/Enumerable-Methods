02bd2cec19032012fdaf21cc8f3f76c43ab2fbaa
Julian <artjulius@hotmail.com>
2019-08-27 12:17:20 -0500

    my_each_with_index method completed and passed the test

--- enumerable.rb
+++ enumerable.rb
@@ -5,8 +5,17 @@ module Enumerable
         i = 0
         self.size.times do
             yield(self[i])  
-            i+=1      
+            i += 1      
         end
+    self
+    end
+    
+    def my_each_with_index
+        i=0
+          self.size.times do
+            yield(self[i], i)
+            i += 1
+        end 
     self
     end
 end
@@ -14,10 +23,16 @@ end
 #========TESTING===========
 
 #=======my_each method test========
-    array=[6,4,8,14,56,12,4,5,8]
+#   array=[6,4,8,14,56,12,4,5,8]
 
-    array.my_each do |num|
-        num *= 2
-        print "#{num} " 
-    end
+#   array.my_each do |num|
+#        num *= 2
+#        print "#{num} " 
+#   end
 #==================================
+
+#=======my_each_with_index method test========
+    array=[6,4,8,14,56,12,4,5,8]
+    array.my_each_with_index do |value, index|
+        puts "Array index #{index} has the value #{value}"
+    end
