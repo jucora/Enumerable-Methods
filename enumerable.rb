9f1067a20e119403f3ae847673297db4c670764f
Julian <artjulius@hotmail.com>
2019-08-27 12:28:01 -0500

    my_select method completed and passed the test

--- enumerable.rb
+++ enumerable.rb
@@ -11,18 +11,30 @@ module Enumerable
     end
     
     def my_each_with_index
-        i=0
+        i = 0
           self.size.times do
             yield(self[i], i)
             i += 1
         end 
     self
     end
+    
+    def my_select
+        arr = []
+        self.my_each do |x|
+            if yield x
+                arr.push x
+            end
+        end
+        arr
+    end
 end
 
-#========TESTING===========
+#========================TESTING INSTRUCTIONS=============================
+#Each test is organized in order, uncomment the test block to see the test
+#=========================================================================
 
-#=======my_each method test========
+#=======MY EACH METHOD TEST========
 #   array=[6,4,8,14,56,12,4,5,8]
 
 #   array.my_each do |num|
@@ -31,8 +43,18 @@ end
 #   end
 #==================================
 
-#=======my_each_with_index method test========
-    array=[6,4,8,14,56,12,4,5,8]
-    array.my_each_with_index do |value, index|
-        puts "Array index #{index} has the value #{value}"
-    end
+
+#=======MY EACH WITH INDEX METHOD TEST=====================
+#    array=[6,4,8,14,56,12,4,5,8]
+#    array.my_each_with_index do |value, index|
+#        puts "Array index #{index} has the value #{value}"
+#    end
+#==========================================================
+
+
+#===============MY SELECT METHOD TEST==================
+#    array=[6,2,7,14,9,12,4,5,8]
+#    result = array.my_select do |n| n % 2 != 0 end
+#    p result
+
+
