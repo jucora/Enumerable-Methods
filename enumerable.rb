5cc66af7016b5730f03c70c0c5f386a5c054f428
Julian <artjulius@hotmail.com>
2019-08-27 11:52:10 -0500

    enumerable.rb file created and my_each method passed the test

---
+++ enumerable.rb
@@ -0,0 +1,23 @@
+#======METHODS============
+
+module Enumerable
+    def my_each
+        i = 0
+        self.size.times do
+            yield(self[i])  
+            i+=1      
+        end
+    self
+    end
+end
+
+#========TESTING===========
+
+#=======my_each method test========
+    array=[6,4,8,14,56,12,4,5,8]
+
+    array.my_each do |num|
+        num *= 2
+        print "#{num} " 
+    end
+#==================================
